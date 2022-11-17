407b3e7fa88833e31ecbdff0401d17a08eca37b1
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-17 00:01:56 -0300

    requisito 7

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,5 +1,11 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (param) => {
+  if(!param) throw new Error ('ID não informado');
+
+  const url = `https://api.mercadolibre.com/items/${param}`;
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
 };
 
 export const fetchProductsList = async (computador) => {
