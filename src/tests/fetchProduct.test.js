635a70e9134fa73d8f92c7abaf08f5f435baa0d2
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-16 23:46:44 -0300

    requisito 6

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -1,6 +1,5 @@
 import './mocks/fetchSimulator';
 import { fetchProduct, fetchProductsList } from '../helpers/fetchFunctions';
-import product from './mocks/product';
 import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
@@ -8,18 +7,22 @@ describe('Teste a função fetchProduct', () => {
   it('testa se fetchProduct é função', () => {
     expect(typeof fetchProduct).toBe('function');
   });
+
   it('Testa se fetch foi chamado ao executar fetchProductList', async () => {
     await fetchProductsList('computador');
     expect(fetch).toHaveBeenCalled();
   });
+
   it('Testa fetchProductList com computador', async () => {
     await fetchProductsList('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
-  // it('Testa se o return de fetchProductList é um objeto em estrutura de dados', async () => {
-  //   const object = await fetchProductsList('computador');
-  //   expect(object.results).toHaveBeenCalled(computadorSearch);
-  // });
+
+  it('Testa se o return de fetchProductList é um objeto em estrutura de dados', async () => {
+    const object = await fetchProductsList('computador');
+    expect(object).toEqual(computadorSearch);
+  });
+
   it('Testa fetchProductList sem param se retorna erro "Termo de busca não informado"', async () => {
     expect(fetchProductsList()).rejects.toThrow(Error);
   });
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,19 +1,32 @@
 import './mocks/fetchSimulator';
-import { fetchProductsList } from '../helpers/fetchFunctions';
+import { fetchProduct, fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
+import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProduct).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
 
+  it('Teste se o retorno da função fetchProduct com o argumento do produto "MLB1405519561" é uma estrutura de dados igual ao objeto produto que já está importado no arquivo.', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(Object).toEqual(product);
+  });
+
+  it('Teste se, ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: "ID não informado"', async () => {
+    const msgError = 'ID não informado';
+    await expect(fetchProduct()).rejects.toThrow(msgError);
   });
 
   // it('...', () => {
