9658a2d1ee0bfaf1df739fc5de30d67798de5f4a
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-17 18:12:48 -0300

    tentei aqui mas deu ruim vou tentar terminar durante a semana os 100%

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,13 +1,5 @@
-export const getAddress = async (cep) => {
-  const urlOne = `https://cep.awesomeapi.com.br/json/${cep}`;
-  const urlTwo = `https://brasilapi.com.br/api/cep/v2/${cep}`;
-
-  const response = await Promise.any([
-    fetch(urlOne),
-    fetch(urlTwo),
-  ]);
-  const data = await response.json();
-  return data;
+export const getAddress = () => {
+  // seu código aqui
 };
 
 export const searchCep = () => {
