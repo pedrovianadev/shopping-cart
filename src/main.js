4af5a63d1b445467af487cefbc233a9061ac277a
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-16 22:23:45 -0300

    acho que o 4 foi mas o cypress não tá indo

--- src/main.js
+++ src/main.js
@@ -7,9 +7,25 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const makeList = document.querySelector('.products');
 
+const loading = () => {
+  const load = document.createElement('h1');
+  load.innerText = 'carregando...';
+  load.className = 'loading';
+  makeList.appendChild(load);
+};
+
+const removeLoading = () => {
+  const remove = document.querySelector('.loading');
+  load.remove();
+};
+
+loading();
+
 const list = await fetchProductsList('computador');
 
 list.forEach((element) => {
   const productItem = createProductElement(element);
   makeList.appendChild(productItem);
 });
+
+removeLoading();
