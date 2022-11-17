7769c736ad9d3e1d2540f25fca0ff3f678e00652
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-16 22:36:01 -0300

    acho que agora a 4 foi

--- src/main.js
+++ src/main.js
@@ -7,12 +7,19 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const makeList = document.querySelector('.products');
 
-const loading = () => {
+function loading() {
   const load = document.createElement('h1');
   load.innerText = 'carregando...';
   load.className = 'loading';
   makeList.appendChild(load);
-};
+}
+
+function removeLoad() {
+  const load = document.querySelector('.loading');
+  load.remove();
+}
+
+loading();
 
 const list = await fetchProductsList('computador');
 
@@ -20,3 +27,5 @@ list.forEach((element) => {
   const productItem = createProductElement(element);
   makeList.appendChild(productItem);
 });
+
+removeLoad();
